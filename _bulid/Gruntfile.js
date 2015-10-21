6e4d9cc86d2c18a35206b42362bd203a9dddaf42
wangxiuwen <634545374@qq.com>
2015-10-21 19:03:27 +0800

    init

---
+++ _bulid/Gruntfile.js
@@ -0,0 +1,49 @@
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        //Read the package.json (optional)
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Metadata.
+        meta: {
+            basePath: '../',
+            srcPath: '../assets/sass/',
+            deployPath: '../assets/css/'
+        },
+
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                '* Copyright (c) <%= grunt.template.today("yyyy") %> ',
+
+        // Task configuration.
+        sass: {
+            dist: {
+                files: {
+                    '<%= meta.deployPath %>main.css': '<%= meta.srcPath %>main.scss'
+                },
+                options: {
+                    sourcemap: 'true',
+		    style:"compressed"
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: [
+                    '<%= meta.srcPath %>/**/*.scss'
+                ],
+                tasks: ['sass']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['sass']);
+};
